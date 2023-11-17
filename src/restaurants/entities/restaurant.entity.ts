4d01a612890ec33428d940a9f7e22cdc3cb3fdeb
JHL71 <neko0717@naver.com>
2023-11-17 19:34:03 +0900

    restarant module

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,7 +1,8 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsOptional, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -12,21 +13,19 @@ export class Restaurant {
 
   @Field(type => String)
   @Column()
+  @IsString()
+  @Length(5, 15)
   name: string;
 
-  @Field(type => Boolean)
-  @Column()
+  @Field(type => Boolean, { defaultValue: true })
+  @Column({ default: true })
+  @IsOptional()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(type => String)
   @Column()
+  @IsString()
   address: string;
 
-  @Field(type => String)
-  @Column()
-  owner: string;
-
-  @Field(type => String)
-  @Column()
-  categoryName: string;
 }
